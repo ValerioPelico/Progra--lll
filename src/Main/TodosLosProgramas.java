f87ce9fe6699f27eaaae8be03f93b7dc98c7a52f
ekt <valeriopelico>
2019-05-17 00:43:42 -0600

    menu de unificacion de laboratorios de programacion 3

--- src/Main/TodosLosProgramas.java
+++ src/Main/TodosLosProgramas.java
@@ -6,17 +6,20 @@
 
 package Main;
 import ListasSimples.Lista;
-import EstructuraColas.Cola;
-import EstructuraPilas.Pila;
-import Nomina.Departamentos_empleados;
-import TorresHanoi.TorreHanoi;
-import arbolBB.Vistaa;
-import java.util.*;
-import planilla_ventana.Planilla_ventana;
+
+import Nomina.Departamentos_empleados; // importamos los paquetes donde se encuntran las clases que mandaremos a llamar
+import TorresHanoi.TorreHanoi;         // importamos los paquetes donde se encuntran las clases que mandaremos a llamar
+import arbolBB.Vistaa;                 // importamos los paquetes donde se encuntran las clases que mandaremos a llamar
+import Pila.Pila;                      // importamos los paquetes donde se encuntran las clases que mandaremos a llamar 
+import ProgramaCola.ProyectoCola;      // importamos los paquetes donde se encuntran las clases que mandaremos a llamar
+import java.util.*;                    
+import javax.swing.JOptionPane;           
+import planilla_ventana.Planilla_ventana;     // importamos los paquetes donde se encuntran las clases que mandaremos a llamar
+
 /**
  *
- * @autor Angel Ruiz
- * 0901-16-17865
+ * @autor Valerio Pelicó
+ * 0901-16-11957
  */
 public class TodosLosProgramas {
     
@@ -27,49 +30,47 @@ public class TodosLosProgramas {
     public static void main(String[] args) {
         // TODO code application logic here.
         Scanner teclado = new Scanner(System.in);
-        int opcion;
+        int opcion=0;
            
             
         do{
-            System.out.println("\n\n");
-            System.out.println("\t   _____________________");
-            System.out.println("\t\tMENU PRINCIPAl");
-            System.out.println("\t   ---------------------");
-            System.out.println("\t1. Nominas");
-            System.out.println("\t2. Nominas con Base de Datos");
-            System.out.println("\t3. Listas Simples");
-            System.out.println("\t4. Pilas");
-            System.out.println("\t5. Torres De Hanoi");
-            System.out.println("\t6. Colas");
-            System.out.println("\t7. Arboles ABB");
-            System.out.println("\t8. Salir");
+            //JOptionPane.showMessageDialog(null, "Bienvenido: seleccione un programa" );
+            System.out.println("Todos los Progrmas:\n");
+            System.out.println("1. Nominas");
+            System.out.println("2. Nominas con Base de Datos");
+            System.out.println("3. Listas");
+            System.out.println("4. Pilas");
+            System.out.println("5. Torres De Hanoi");
+            System.out.println("6. Colas");
+            System.out.println("7. Arboles ABB");
+            System.out.println("8. Salir\n");
             opcion = teclado.nextInt();
             
             switch(opcion){
                 case 1: 
-                    Departamentos_empleados.main(args);
+                    Departamentos_empleados.main(args); // llama el metodo main de la clase Departamento_empleados
                     break;
                 case 2:
-                    Planilla_ventana.main(args);
+                    Planilla_ventana.main(args);        // llama el main de la clase planilla_ventana
                     break;
                 case 3:
-                    Lista.main(args);
+                    Lista.main(args);                   // llama el main de la clase lista
                     break;
                 case 4:
-                    Pila.main(args);
+                    Pila.main(args);                     // llama el main de la clase pila
                     break;
                 case 5: 
-                    TorreHanoi.main(args);
+                    TorreHanoi.main(args);               // llama el main de la clase torres de hanoi
                     break;
-                case 6:
-                    Cola.main(args);
+                case 6:ProyectoCola.main(args);           // llama el main de la clase cola 
+                    
                     break;
-                case 7: 
+                case 7:                                   // llama el main de la clase listas
                     Vistaa.main(args);
                 case 8:
                     break;
                 default: 
-                    System.out.println("Opcion no valida");
+                    System.out.println("Opcion no valida");                   
             }
         }while(opcion!=8);
     }
